2fd791450cc568c220fcccc0499b9f66397c0d35
Ahmed <uncannycoderz@gmail.com>
2024-12-30 03:14:49 +0500

    new changes

---
+++ main.js
@@ -0,0 +1,76 @@
+// Mcqs array
+const questions= [
+  {
+    type:'survey',
+    text:'Please Enter Your Email',
+    placeholder:'email@email.com',
+    templateId:'email',
+    required: true,
+    nextIndex:'1',
+  },
+  {
+    type:'survey',
+    text:'Please Name the Influencer',
+    placeholder:'E.g Christiano Ronaldo',
+    templateId:'influencer',
+    required: true,
+    nextIndex:'1',
+  },
+  {
+    type:'survey',
+    text:'Please Share Your Feedback',
+    placeholder:'"We would love to hear your feedback!"',
+    templateId:'feedback',
+    required: true,
+    nextIndex:'1',
+  },
+  {
+    type: 'mcq',
+    text: "What didn't you like about the shopping experience?",
+    templateId: 'mcqTemplate1',
+    options: [
+      { text: 'The website was hard to navigate', nextIndex: 3 },
+      { text: 'The website was slow', nextIndex: 3 },
+      { text: 'The checkout process was confusing', nextIndex: 3 },
+      { text: 'There were too many pop-ups or ads', nextIndex: 3 },
+    ],
+    showFifthOption: false,
+  },
+  {
+    type: 'mcq',
+    text: "Wehere did you hear about us?",
+    templateId: 'mcqTemplate2',
+    options: [
+      { text: 'Google', nextIndex: 5 },
+      { text: 'Instagram/Facebook', nextIndex: 5 },
+      { text: 'From a Friend', nextIndex: 5 },
+      { text: 'Influencer', nextIndex: 4 },
+    ],
+    showFifthOption: false,
+  },
+  {
+    type: 'mcq',
+    text: "How would you rate our checkot process?",
+    templateId: 'mcqTemplate3',
+    options: [
+      { text: 'Very Easy', nextIndex: 6 },
+      { text: 'Easy', nextIndex: 6 },
+      { text: 'Difficult', nextIndex: 6 },
+      { text: 'Very Difficult', nextIndex: 6 },
+    ],
+    showFifthOption: false,
+  },
+  {
+    type: 'mcq',
+    text: "Would you recommend our website to others?",
+    templateId: 'mcqTemplate4',
+    options: [
+      { text: 'Definately', nextIndex: 7 },
+      { text: 'Probably', nextIndex: 7 },
+      { text: 'Not Sure', nextIndex: 7 },
+      { text: 'Probably Not', nextIndex: 7 },
+      { text: 'Definately Not', nextIndex: 7 },
+    ],
+    showFifthOption: true,
+  },
+]
